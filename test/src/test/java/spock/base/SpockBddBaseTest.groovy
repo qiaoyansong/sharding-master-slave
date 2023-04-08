81694fd61d08046b99ffd29493a9285886d9ad26
qiaoyansong <qiaoyansong@didiglobal.com>
2023-04-08 15:35:48 +0800

    init

--- test/src/test/java/spock/base/SpockBddBaseTest.groovy
+++ test/src/test/java/spock/base/SpockBddBaseTest.groovy
@@ -1,14 +1,6 @@
 package spock.base
 
-import com.sharding.master.slave.api.HelloRpcService
-import com.sharding.master.slave.biz.helper.Do2DtoHelper
-import com.sharding.master.slave.biz.impl.HelloServiceImpl
-import com.sharding.master.slave.biz.service.HelloService
-import com.dal.mapper.UserMapper
-import com.sharding.master.slave.service.HelloRpcServiceImpl
-import com.sharding.master.slave.test.utils.MockHelper
-import org.mapstruct.factory.Mappers
-import org.mockito.Mockito
+
 import spock.lang.Specification
 /**
  * @author ：Qiao Yansong
@@ -17,23 +9,11 @@ import spock.lang.Specification
  */
 class SpockBddBaseTest extends Specification {
 
-    HelloRpcService helloRpcService;
-    HelloService helloService;
-    UserMapper userMapper;
-    Do2DtoHelper do2DtoHelper;
 
     {
         // 确定的上下游服务
-        helloRpcService = new HelloRpcServiceImpl();
-        helloService = new HelloServiceImpl();
-        do2DtoHelper = Mappers.getMapper( Do2DtoHelper.class);;
 
         // 不确定的上下游服务，直接mock出来
-        userMapper = Mockito.mock(UserMapper.class);
-
-        MockHelper.mockField(helloRpcService, "helloService", helloService);
-        MockHelper.mockField(helloService, "do2DtoHelper", do2DtoHelper);
-        MockHelper.mockField(helloService, "userMapper", userMapper);
     }
     
 }
